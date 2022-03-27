679647b9a517467ba5c195fb1e4800afb5eae94f
hugosenari <hugosenari@gmail.com>
2022-03-27 18:16:35 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-gentabs-master.ref   = "refs/heads/master";
   inputs.src-gentabs-master.owner = "lcrees";
   inputs.src-gentabs-master.repo  = "gentabs";
+  inputs.src-gentabs-master.dir   = "";
   inputs.src-gentabs-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
