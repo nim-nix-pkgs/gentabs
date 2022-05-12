3f069a82391276f16f0a13ea39c6271100af41a0
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:52 -0300

    chore: re index gentabs

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."gentabs-master".dir   = "master";
+  inputs."gentabs-master".owner = "nim-nix-pkgs";
+  inputs."gentabs-master".ref   = "master";
+  inputs."gentabs-master".repo  = "gentabs";
+  inputs."gentabs-master".type  = "github";
+  inputs."gentabs-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."gentabs-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
